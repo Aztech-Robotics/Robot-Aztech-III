ad71d0a060ad88554ce28643b0685ff65884869c
GustavoBC2007 <gustavobc2007@gmail.com>
2020-10-16 11:22:41 -0500

    Segundo cambio método despedida

--- HolaMundo.java
+++ HolaMundo.java
@@ -8,10 +8,16 @@ public class HolaMundo {
     System.out.println("Primer programa en Java");
 
     saludar(); // Llamada al método "saludar"
+    despedir(); // Llamada al método "despedir"
   }
 
   // Método: "saludar"
   public static void saludar() {
     System.out.println("Hola, les manda saludos Armando");
+  }
+
+  // Método "despedir al usuario"
+  public static void despedir(){
+    System.out.println("Adiós, hasta la próxima");
   }
 }
