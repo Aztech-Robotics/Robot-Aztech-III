af14954d445de32825812b6ac88c0c3b361b9e96
Armando-Alamilla <armando_alamilla10@hotmail.com>
2020-10-16 11:11:29 -0500

    Método saludar

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,17 @@
+// Objeto: "HolaMundo"
+
+// Clase de nuestro objeto
 public class HolaMundo {
+
+  // Método: "main"
   public static void main(String[] args) {
-    System.out.println("Hola Mundo");
+    System.out.println("Primer programa en Java");
+
+    saludar(); // Llamada al método "saludar"
+  }
+
+  // Método: "saludar"
+  public static void saludar() {
+    System.out.println("Hola, les manda saludos Armando");
   }
 }
