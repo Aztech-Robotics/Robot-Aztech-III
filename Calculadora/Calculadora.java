92bdd59c3d3d1ab2b3f06bff2ad49bfcfef26ca6
GustavoBC2007 <gustavobc2007@gmail.com>
2020-10-18 10:59:11 -0500

    Hice una modificación en el método multiplicar de el paquete calculadora

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,5 +1,6 @@
 package Calculadora;
-
+  // importar la entrada por teclado
+import java.util.Scanner;
 // Clase del obeto calculadora
 public class Calculadora {
   // método para sumar
@@ -12,4 +13,19 @@ public class Calculadora {
   // método resta
 
   // método multiplicación
+public static int multiplicar() {
+    Scanner teclado = new Scanner (System.in);
+    int dato,num,mul;
+    System.out.println("Ingresa un numero entero que deses multiplicar");
+    dato = teclado.nextInt (); 
+    System.out.println("Ingresa 0 para finalizar");
+    int i=1;
+    while (dato!=0) {
+      System.out.println("Ingresa otro numero entero que deses multiplicar");
+    num = teclado.nextInt (); 
+    int mul = dato*num;
+    return mul;
+   i++;
+  }
+}
 }
