81a8a1188f6440484109f10cff5887b21126aeaf
Armando-Alamilla <armando_alamilla10@hotmail.com>
2020-10-22 11:39:43 -0500

    Don't repeat yourself

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,46 +1,19 @@
 package Calculadora;
-  // importar la entrada por teclado
-import java.util.Scanner;
+
 // Clase del obeto calculadora
 public class Calculadora {
   // método para sumar
-  public int sumar() {
-    int res = 1 + 3 + 5;
-
-    return res;
+  public int sumar(int a, int b) {
+    return a + b;
   }
 
-  // método resta
-  public int restar() {
-    Scanner teclado = new Scanner (System.in);
-    int a, b, r;
-    System.out.println("Ingrese el número A: ");
-    a = teclado.nextInt();
-    System.out.println("Ingrese el número B: ");
-    b = teclado.nextInt();
-    System.out.println("Su resultado es: ");
-    r = a - b;
-    teclado.close();
-    return r;
-    
+  // método para multiplicar
+  public int multiplicar(int a, int b) {
+    return a * b;
   }
 
-
-
-  // método multiplicación
-public static int multiplicar() {
-    Scanner teclado = new Scanner (System.in);
-    int dato,num,mul;
-    System.out.println("Ingresa un numero entero que deses multiplicar");
-    dato = teclado.nextInt (); 
-    System.out.println("Ingresa 0 para finalizar");
-    int i=1;
-    while (dato!=0) {
-      System.out.println("Ingresa otro numero entero que deses multiplicar");
-    num = teclado.nextInt (); 
-    int mul = dato*num;
-    return mul;
-   i++;
+  // método para restar
+  public int restar(int a, int b) {
+    return a - b;
   }
-}
 }
--- HolaMundo.java
+++ HolaMundo.java
@@ -1,23 +1,39 @@
 
-// Importamos la clase calculadora
+// Importar clase Calculadora para hacer operaciones
 import Calculadora.Calculadora;
 
-// Objeto: "HolaMundo"
-// Clase de nuestro objeto
-public class HolaMundo {
+// importar clase Scanner para entrada por teclado
+import java.util.Scanner;
 
+// Clase del objeto HolaMundo
+public class HolaMundo {
   // Método: "main"
   public static void main(String[] args) {
-    System.out.println("Primer programa en Java");
-
-    // Creamos el objeto de la clase Calculadora
     // Tipo de dato / nombre del objeto / operador new / constructor
-    Calculadora calculadora = new Calculadora();
-    System.out.println(calculadora.sumar()); // llamada al método "sumar" del objeto calculadora
-    System.out.println(calculadora.restar()); // Llamada al metodo "restar" de la clase "Calculadora"
-    saludar(); // Llamada al método "saludar"
-    despedir(); // Llamada al método "despedir"
-    Bienvenido(); // Llamada al metodo "Bienvenido"
+    Calculadora calculadora = new Calculadora(); // Crear objeto de la clase Calculadora
+    Scanner teclado = new Scanner(System.in); // Crear objeto de la clase Scanner
+
+    // Ingresando el numero A
+    System.out.print("Ingrese el número A: ");
+    int a = teclado.nextInt();
+
+    // Ingresar el numero B
+    System.out.print("Ingrese el número B: ");
+    int b = teclado.nextInt();
+
+    // Procesar los datos mediante los métodos del objeto calculadora
+    int suma = calculadora.sumar(a, b);
+    int multiplicacion = calculadora.multiplicar(a, b);
+    int resta = calculadora.restar(a, b);
+
+    // Mostrar los resultados
+    System.out.println("La suma es: " + suma);
+    System.out.println("La multiplicacion es: " + multiplicacion);
+    System.out.println("La resta es: " + resta);
+
+    // saludar(); // Llamada al método "saludar"
+    // despedir(); // Llamada al método "despedir"
+    // Bienvenido(); // Llamada al metodo "Bienvenido"
   }
 
   // Método: "saludar"
