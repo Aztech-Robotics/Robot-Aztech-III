bc64688789e8c88611cc0f158e77e56253a7bd95
Armando-Alamilla <armando_alamilla10@hotmail.com>
2020-10-30 09:05:09 -0600

    Ejemplo de encapsulamiento y herencia

---
+++ Escuela/Alumno.java
@@ -0,0 +1,13 @@
+package Escuela;
+
+public class Alumno extends Persona {
+  private int matricula;
+
+  public void setEdad(int edad) {
+    this.edad = edad;
+  }
+
+  public int getEdad() {
+    return edad;
+  }
+}
---
+++ Escuela/Persona.java
@@ -0,0 +1,9 @@
+package Escuela;
+
+public class Persona {
+  public int edad;
+
+  public void saludar() {
+    System.out.println("Hola, soy una persona");
+  }
+}
